ac00878579b55e663c3c4d828040e0fc5221bfb1
Jeremy Arr <jeremyarr@gmail.com>
2017-07-19 16:52:23 +1000

    performs version check prior to install

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from setuptools import setup
 import os
+import sys
+PY_VER = sys.version_info
+
+if not PY_VER >= (3, 6):
+    raise RuntimeError("mooq doesn't support Python earlier than 3.6")
 
 here = os.path.abspath(os.path.dirname(__file__))
 
@@ -7,12 +12,16 @@ about = {}
 with open(os.path.join(here, 'mooq', '__version__.py'), 'r') as f:
     exec(f.read(), about)
 
+def read(fname):
+    with open(os.path.join(here, fname), 'r') as f:
+        return str(f.read().strip())
+
 setup(
     name='mooq',
     version=about['__version__'],
     packages=['mooq'],
     description="An asyncio compatible library for interacting with a RabbitMQ AMQP broker",
-    long_description="An asyncio compatible library for interacting with a RabbitMQ AMQP broker",
+    long_description='\n\n'.join((read('README.rst'), read('CHANGELOG.rst'))),
     include_package_data=True,
     install_requires=[
         'pika',
@@ -27,7 +36,7 @@ setup(
              ],
     url="https://github.com/jeremyarr/mooq",
     classifiers = [
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
         'Intended Audience :: Developers',
         'Programming Language :: Python :: 3.6',
